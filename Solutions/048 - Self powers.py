d6006b09fd888e80f0277ce5f2b1d98634c6bddc
ZazenCloud <131422668+ZazenCloud@users.noreply.github.com>
2023-05-25 17:23:22 -0300

    Add files via upload

--- Solutions/048 - Self powers.py
+++ Solutions/048 - Self powers.py
@@ -1,11 +1,7 @@
 total = 0
 
-# Iterate over the numbers from 1 to 1000
 for num in range(1, 1001):
-    # Calculate the exponentiation of the number raised to itself
     expo_num = num ** num
-    # Add the exponentiation result to the total
     total += expo_num
-
-# Print the last 10 digits 
+    
 print(str(total)[-10:])
