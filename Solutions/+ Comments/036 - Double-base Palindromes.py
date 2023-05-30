6720e78f6025f65a57395d2546b3e65abc0140d0
ZazenCloud <131422668+ZazenCloud@users.noreply.github.com>
2023-05-30 20:50:07 -0300

    Add files via upload

---
+++ Solutions/+ Comments/036 - Double-base Palindromes.py
@@ -0,0 +1,21 @@
+def is_palindromic(n):
+    # Convert n to base 10 and base 2 strings
+    base2 = bin(n)[2:]  # Remove the '0b' prefix
+    base10 = str(n)
+    # Check if both strings are equal to their reversed versions
+    return base10 == base10[::-1] and base2 == base2[::-1]
+
+
+def find_sum(limit):
+    total = 0
+    # Loop through all the numbers less than limit
+    for n in range(limit):
+        # Check if n is palindromic in both bases
+        if is_palindromic(n):
+            # Add n to the sum
+            total += n
+    # Return the sum
+    return total
+
+
+print(find_sum(1000000))
