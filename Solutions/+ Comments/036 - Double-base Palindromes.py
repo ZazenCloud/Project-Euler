7f23d7e2e1ab89fe861a811b2828a0ebab0cda3c
ZazenCloud <131422668+ZazenCloud@users.noreply.github.com>
2023-05-30 20:56:13 -0300

    Add files via upload

--- Solutions/+ Comments/036 - Double-base Palindromes.py
+++ Solutions/+ Comments/036 - Double-base Palindromes.py
@@ -1,7 +1,7 @@
 def is_palindromic(n):
     # Convert n to base 10 and base 2 strings
-    base2 = bin(n)[2:]  # Remove the '0b' prefix
     base10 = str(n)
+    base2 = bin(n)[2:]  # Remove the '0b' prefix
     # Check if both strings are equal to their reversed versions
     return base10 == base10[::-1] and base2 == base2[::-1]
 
